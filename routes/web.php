df51e8fa75ad5b75954bf6e7abbdb9e1c4243ade
Ozodbek <qurbonovo2008@gmail.com>
2025-12-09 15:49:01 +0500

    add new file balde file

--- routes/web.php
+++ routes/web.php
@@ -2,4 +2,12 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::view('/{any}', 'dashboard')->where('any', '.*');
+// Route::view('/{any}', 'dashboard')->where('any', '.*');
+
+
+// website url
+
+Route::get('/' , function (){
+    return view('index');
+});
+
